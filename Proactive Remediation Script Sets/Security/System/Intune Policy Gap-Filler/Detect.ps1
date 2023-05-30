3a29f4fd79de82195988114af41db3bb1fdf1a54
Morgan <morgan.wicks@icloud.com>
2023-05-30 15:31:41 +1000

    Update Detect.ps1
    Add Memory Integrity to DETECT Script.

--- Proactive Remediation Script Sets/Security/System/Intune Policy Gap-Filler/Detect.ps1
+++ Proactive Remediation Script Sets/Security/System/Intune Policy Gap-Filler/Detect.ps1
@@ -34,13 +34,16 @@ Function Test-RegistryValue {
 
 # Configure Data
 $data = @(
-    "HKLM:\\SYSTEM\CurrentControlSet\Control\Lsa",                      "disabledomaincreds",           "DWord",    "1",       
-    "HKLM:\\SYSTEM\CurrentControlSet\Control\Lsa",                      "RunAsPPL",                     "DWord",    "1",
-    "HKLM:\\SOFTWARE\Policies\Microsoft\Windows\Installer",             "AlwaysInstallElevated",        "DWord",    "0",
-    "HKLM:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections",   "NC_ShowSharedAccessUI",        "DWord",    "0",
-    "HKLM:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections",   "NC_StdDomainUserSetLocation",  "DWord",    "1",
-    "HKLM:\\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services",  "UserAuthentication",           "DWord",    "1",
-    "HKLM:\\SOFTWARE\Policies\Microsoft Services\AdmPwd",               "AdmPwdEnabled",                "DWord",    "1"
+    "HKLM:\\SYSTEM\CurrentControlSet\Control\Lsa",                                                      "disabledomaincreds",           "DWord",    "1",       
+    "HKLM:\\SYSTEM\CurrentControlSet\Control\Lsa",                                                      "RunAsPPL",                     "DWord",    "1",
+    "HKLM:\\SOFTWARE\Policies\Microsoft\Windows\Installer",                                             "AlwaysInstallElevated",        "DWord",    "0",
+    "HKLM:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections",                                   "NC_ShowSharedAccessUI",        "DWord",    "0",
+    "HKLM:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections",                                   "NC_StdDomainUserSetLocation",  "DWord",    "1",
+    "HKLM:\\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services",                                  "UserAuthentication",           "DWord",    "1",
+    "HKLM:\\SOFTWARE\Policies\Microsoft Services\AdmPwd",                                               "AdmPwdEnabled",                "DWord",    "1",
+    "HKLM:\\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity",    "Enabled",                      "DWord",    "1",       
+    "HKLM:\\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity",    "Locked",                       "DWord",    "1",   
+    "HKLM:\\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity",    "HVCIMATRequired",              "DWord",    "0",
 )
 
 try {
